419fb75e89ed576090a84e1daa1d4921f47641da
Yanyu02051215 <dcjcsk0205@gmail.com>
2019-10-26 21:02:40 +0900

    under-line2

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', (function() {
 
   var search_list = $("#user-search-result");
-  var member_list = $(".chat-group-user__name");
+  var member_list = $("#chat-group-users");
 
 
 
