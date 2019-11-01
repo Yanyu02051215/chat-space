bd1d11228683568eea7c5fd86a2b8d43ad840899
Yanyu02051215 <dcjcsk0205@gmail.com>
2019-11-01 18:52:32 +0900

    自動更新2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -75,7 +75,7 @@ $(function() {
         })
         $('.main__message').animate({scrollTop: $('.main__message')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
       })
-      .fail(function (messages) {
+      .fail(function () {
         alert('自動更新に失敗しました');//ダメだったらアラートを出す
       });
     }
