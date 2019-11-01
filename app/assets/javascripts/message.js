375adacdd07d905e72444b4a6950649411272af7
Yanyu02051215 <dcjcsk0205@gmail.com>
2019-11-01 18:45:57 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -59,9 +59,8 @@ $(function() {
   });
   var reloadMessages = function () {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
-      var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
+      var last_message_id = $('.main__message__box:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
       // var group_id = $(".group").data("group-id");
-
       $.ajax({ //ajax通信で以下のことを行う
         url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
         type: 'get', //メソッドを指定
@@ -72,11 +71,11 @@ $(function() {
         var insertHTML = '';//追加するHTMLの入れ物を作る
         messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
           insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
-          $('.messages').append(insertHTML);//メッセージを追加
+          $('.main__message').append(insertHTML);//メッセージを追加
         })
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+        $('.main__message').animate({scrollTop: $('.main__message')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
       })
-      .fail(function () {
+      .fail(function (messages) {
         alert('自動更新に失敗しました');//ダメだったらアラートを出す
       });
     }
